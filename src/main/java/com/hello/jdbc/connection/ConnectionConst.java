caedd3255b98d6d661437d5a46172e666a6c9134
ffolabear <ffolabear@gmail.com>
2024-11-12 23:31:49 +0900

    섹션2 예제 추가

---
+++ src/main/java/com/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,8 @@
+package com.hello.jdbc.connection;
+
+//상수고 사용할거라 그냥 abstract 로 선언
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+}
