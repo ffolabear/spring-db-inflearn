e7283bff48f6ef6c0beab17420f9f71cea4da50b
ffolabear <ffolabear@gmail.com>
2024-11-27 23:43:46 +0900

    섹션6 예제 추가

---
+++ src/test/java/com/hello/jdbc/exception/CheckedTest.java
@@ -0,0 +1,73 @@
+package com.hello.jdbc.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class CheckedTest {
+
+    @Test
+    void checked_catch() {
+        Service service = new Service();
+        //예외를 잡았기 때문에 프로그램 정상 종료
+        service.callCatch();
+    }
+
+    @Test
+    void checked_throw() {
+        Service service = new Service();
+        Assertions.assertThatThrownBy(() -> service.callThrow())
+                .isInstanceOf(MyCheckedException.class);
+    }
+
+    /**
+     * Exception 을 상속받은 예외는 체크 예외가 된다.
+      */
+    static class MyCheckedException extends Exception{
+        public MyCheckedException(String message) {
+            super(message);
+        }
+    }
+
+    /**
+     * Checked 예외는
+     * 예외를 잡아서 처리하거나, 던지거나 둘중 하나를 필수로 선택해야 한다.
+     */
+    static class Service{
+        Repository repository = new Repository();
+
+        /**
+         * 예외를 잡아서 처리하는 코드
+         */
+        public void callCatch() {
+            //예외를 던지지 않을거면 잡아야함!
+            try {
+                repository.call();
+            } catch (MyCheckedException e) {
+                //exception 을 스택트레이스에 출력하려면 3번째 파라미터로 추가
+                log.error("예외 처리, message={}", e.getMessage(), e);
+            }
+        }
+
+        /**
+         * 체크 예외를 밖으로 던지는 코드
+         * 체크 예외는 예외를 잡지 않고 밖으로 던지려면 throws 예외를 메서드에 필수로 선언해야한다.
+         * @throws MyCheckedException
+         */
+        public void callThrow() throws MyCheckedException {
+            repository.call();
+        }
+    }
+
+    static class Repository {
+
+        //checked 예외는 컴파일러가 체크해주기 떄문에 메소드 시그니쳐에 선언해야함
+        //안하면 컴파일 에러!
+        public void call() throws MyCheckedException {
+            throw new MyCheckedException("ex");
+        }
+
+    }
+
+}
