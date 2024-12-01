eeacec997dcdc891f283b4c31d23282e47fb51b0
ffolabear <ffolabear@gmail.com>
2024-12-02 00:04:18 +0900

    섹션6 예제 추가

--- src/test/java/com/hello/jdbc/exception/UnCheckedAppTest.java
+++ src/test/java/com/hello/jdbc/exception/UnCheckedAppTest.java
@@ -29,15 +29,15 @@ public class UnCheckedAppTest {
         Repository repository = new Repository();
         NetworkClient networkClient = new NetworkClient();
 
-        public void logic() throws SQLException, ConnectException {
+        public void logic()  {
             repository.call();
             networkClient.call();
         }
 
         static class NetworkClient {
 
-            public void call() throws ConnectException {
-                throw new ConnectException("연결 실패");
+            public void call() {
+                throw new RuntimeConnectException("연결 실패");
             }
         }
 
@@ -45,8 +45,31 @@ public class UnCheckedAppTest {
 
     static class Repository {
 
-        public void call() throws SQLException {
+        public void call() {
+            try {
+                runSQL();
+            } catch (SQLException e) {
+                throw new RuntimeSQLException(e);
+            }
+        }
+
+        public void runSQL() throws SQLException {
             throw new SQLException("ex");
+        }
+    }
+
+
+    static class RuntimeConnectException extends RuntimeException {
+
+        public RuntimeConnectException(String message) {
+            super(message);
+        }
+    }
+
+    static class RuntimeSQLException extends RuntimeException {
+
+        public RuntimeSQLException(Throwable cause) {
+            super(cause);
         }
     }
 }
